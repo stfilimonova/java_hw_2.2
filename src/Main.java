7f808c572f33bfbf6784b357cf2a815eaac18e7b
stfilimonova <stfilimonova@mail.ru>
2023-03-14 05:51:14 +0300

    Main file changed

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
-public class            Main {
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        double account = 150;
+        double refill = 1200;
+        int bonus;
+
+        if (refill > 1000) {
+            bonus = (int) (refill / 100);
+            refill = refill + bonus;
+        } else {
+            bonus = 0;
+        }
+
+        System.out.println("Количество бонусов за пополнение: " + bonus);
+        System.out.println("Итоговый счет: " + (account + refill));
     }
 }
